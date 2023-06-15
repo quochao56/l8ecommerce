06aa37d6bb3a171580402535ec66548ea79dfd53
quochao56 <quochao2k2@gmail.com>
2023-06-15 16:15:33 +0700

    add HomeComponent.php, home-component; set up base

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',HomeComponent::class);
+
+Route::get('/shop',ShopComponent::class); 
+
+Route::get('/cart',CartComponent::class); 
+
+Route::get('/checkout',CheckoutComponent::class); 
