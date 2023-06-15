b147a54608c6e23714db01e98a614c075453aecf
quochao56 <quochao2k2@gmail.com>
2023-06-15 21:57:52 +0700

    change form login, register (need to improve)

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -98,11 +98,13 @@
                                                         <a title="Dashboard"
                                                             href="{{ route('admin.dashboard') }}">Dashboard</a>
                                                     </li>
-													<li class="menu-item">
-														<a href="{{ route('logout') }}"
-															onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-													</li>
-                                                    <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                                    {{-- Logout --}}
+                                                    <li class="menu-item">
+                                                        <a href="{{ route('logout') }}"
+                                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id="logout-form" method="POST"
+                                                        action="{{ route('logout') }}">
                                                         @csrf
                                                     </form>
                                                 </ul>
@@ -118,13 +120,16 @@
                                                         <a title="Dashboard"
                                                             href="{{ route('user.dashboard') }}">Dashboard</a>
                                                     </li>
+                                                    {{-- Logout --}}
                                                     <li class="menu-item">
-														<a href="{{ route('logout') }}"
-															onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-													</li>
-                                                    <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                                        <a href="{{ route('logout') }}"
+                                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id="logout-form" method="POST"
+                                                        action="{{ route('logout') }}">
                                                         @csrf
                                                     </form>
+
                                                 </ul>
                                             </li>
                                         @endif
