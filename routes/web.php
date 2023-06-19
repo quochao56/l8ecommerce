9b17b10bdfae20ed662d4ad55d9418d882759577
quochao56 <quochao2k2@gmail.com>
2023-06-19 16:38:22 +0700

    Admin Edit Category

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
@@ -62,6 +63,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
         Route::get('/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
         Route::get('/categories', AdminCategoryComponent::class)->name('admin.categories');
         Route::get('/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
+        Route::get('/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
         
     });
 
