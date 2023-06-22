bde98fad9ffe387ef800642b6e54d8f98be0f983
quochao56 <quochao2k2@gmail.com>
2023-06-22 15:21:19 +0700

    Remove Product from Wishlist

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -17,21 +17,34 @@ class ShopComponent extends Component
 
     public $min_price;
     public $max_price;
-    public function mount(){
+    public function mount()
+    {
         $this->sorting = "default";
         $this->pagesize = 12;
         $this->min_price = 1;
         $this->max_price = 1000;
     }
-    public function store($product_id, $product_name, $product_price){
-        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate("App\Models\Product");
-        session()->flash('success_message','Item added in Cart');
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::instance('cart')->add($product_id, $product_name, 1, $product_price)->associate("App\Models\Product");
+        session()->flash('success_message', 'Item added in Cart');
         return redirect()->route('product.cart');
     }
 
-    public function addToWishlist($product_id, $product_name, $product_price){
-        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate("App\Models\Product");
-        $this->emitTo('wishlist-count-component','refreshComponent');
+    public function addToWishlist($product_id, $product_name, $product_price)
+    {
+        Cart::instance('wishlist')->add($product_id, $product_name, 1, $product_price)->associate("App\Models\Product");
+        $this->emitTo('wishlist-count-component', 'refreshComponent');
+    }
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem) {
+            if($witem->id == $product_id) {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-count-component', 'refreshComponent');
+                return;
+            }
+        }
     }
     public function render()
     {
