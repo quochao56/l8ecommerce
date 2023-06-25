c26eb8be902cfb36fb4ef3994c9cd5ff73036c0e
quochao56 <quochao2k2@gmail.com>
2023-06-25 15:38:13 +0700

    update scroll table

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -15,58 +15,64 @@
                     All Orders
                 </div>
                 <div class="panel-body">
-                    @if(Session::has('order_message'))
+                    @if (Session::has('order_message'))
                         <div class="alert alert-success" role="alert">{{ Session::get('order_message') }}</div>
                     @endif
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>OrderId</th>
-                                <th>Subtotal</th>
-                                <th>Discount</th>
-                                <th>Tax</th>
-                                <th>Total</th>
-                                <th>First name</th>
-                                <th>Last name</th>
-                                <th>Mobile</th>
-                                <th>Email</th>
-                                <th>Zipcode</th>
-                                <th>Status</th>
-                                <th>Order Date</th>
-                                <td colspan="2" class="text-center">Action</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($orders as $order)
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
                                 <tr>
-                                    <td>{{ $order->id }}</td>
-                                    <td>${{ $order->subtotal }}</td>
-                                    <td>${{ $order->discount }}</td>
-                                    <td>${{ $order->tax }}</td>
-                                    <td>${{ $order->total }}</td>
-                                    <td>{{ $order->firstname }}</td>
-                                    <td>{{ $order->lastname }}</td>
-                                    <td>{{ $order->mobile }}</td>
-                                    <td>{{ $order->email }}</td>
-                                    <td>{{ $order->zipcode }}</td>
-                                    <td>{{ $order->status }}</td>
-                                    <td>{{ $order->created_at }}</td>
-                                    <td><a href="{{ route('admin.order-details', ['order_id' => $order->id]) }}"
-                                            class="btn btn-info btn-sm">Details</a>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button class="btn btn-success btn-sm dropdown-toggle" type="button"
-                                                data-toggle="dropdown">Status <span class="caret"></span></button>
-                                            <ul class="dropdown-menu">
-                                                <li><a href="#" wire:click.prevent="updateOrderStatus({{ $order->id }},'delivered')">Delivered</a></li>
-                                                <li><a href="#" wire:click.prevent="updateOrderStatus({{ $order->id }},'canceled')">Canceled</a></li>
-                                            </ul>
-                                        </div>
-                                    </td>
+                                    <th>OrderId</th>
+                                    <th>Subtotal</th>
+                                    <th>Discount</th>
+                                    <th>Tax</th>
+                                    <th>Total</th>
+                                    <th>First name</th>
+                                    <th>Last name</th>
+                                    <th>Mobile</th>
+                                    <th>Email</th>
+                                    <th>Zipcode</th>
+                                    <th>Status</th>
+                                    <th>Order Date</th>
+                                    <td colspan="2" class="text-center">Action</td>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($orders as $order)
+                                    <tr>
+                                        <td>{{ $order->id }}</td>
+                                        <td>${{ $order->subtotal }}</td>
+                                        <td>${{ $order->discount }}</td>
+                                        <td>${{ $order->tax }}</td>
+                                        <td>${{ $order->total }}</td>
+                                        <td>{{ $order->firstname }}</td>
+                                        <td>{{ $order->lastname }}</td>
+                                        <td>{{ $order->mobile }}</td>
+                                        <td>{{ $order->email }}</td>
+                                        <td>{{ $order->zipcode }}</td>
+                                        <td>{{ $order->status }}</td>
+                                        <td>{{ $order->created_at }}</td>
+                                        <td><a href="{{ route('admin.order-details', ['order_id' => $order->id]) }}"
+                                                class="btn btn-info btn-sm">Details</a>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button class="btn btn-success btn-sm dropdown-toggle" type="button"
+                                                    data-toggle="dropdown">Status <span class="caret"></span></button>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="#"
+                                                            wire:click.prevent="updateOrderStatus({{ $order->id }},'delivered')">Delivered</a>
+                                                    </li>
+                                                    <li><a href="#"
+                                                            wire:click.prevent="updateOrderStatus({{ $order->id }},'canceled')">Canceled</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                     <div class="pagination-summary">
                         Showing {{ $startIndex }} to {{ $endIndex }} of {{ $totalResults }} results
                     </div>
