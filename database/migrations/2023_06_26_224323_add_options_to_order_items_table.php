556c9c11610ebd65ec6c6decfd9710052cbef70f
quochao56 <quochao2k2@gmail.com>
2023-07-11 15:55:46 +0700

    fix migrate

--- database/migrations/2023_06_26_224323_add_options_to_order_items_table.php
+++ database/migrations/2023_06_26_224323_add_options_to_order_items_table.php
@@ -4,20 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('attribute_values', function (Blueprint $table) {
-            $table->id();
-            $table->bigInteger('product_attribute_id')->unsigned()->nullable();
-            $table->string('value');
-            $table->bigInteger('product_id')->unsigned()->nullable();
-            $table->timestamps();
-            $table->foreign('product_attribute_id')->references('id')->on('product_attributes')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->longText('options')->nullable();
         });
     }
 
